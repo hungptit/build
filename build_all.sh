bb41d2e0f2a590132c592881fca2db3fc94ec65a
Hung Dang <hdang@athenahealth.com>
2018-01-20 20:07:47 -0500

    Update the build script for rocksdb.

--- build_all.sh
+++ build_all.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -euo pipefail				# Use Bash strict mode
-# Download required packages
-./download_source_code.sh http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz bzip2
-./download_source_code.sh http://sqlite.org/2017/sqlite-autoconf-3180000.tar.gz
+
 
 printf "Build CMake\n";
-./build_using_configure.sh CMake > /dev/null
+./build_using_cmake.sh CMake > /dev/null
 
 printf "Build Catch\n";
-./build_using_cmake.sh Catch
+./build_using_cmake.sh Catch2
 
 printf "Build fmt\n"
 ./build_using_cmake.sh fmt "-DFMT_DOC=OFF -DFMT_TEST=OFF" > /dev/null
 
-printf "Build googletest\n"
-./build_using_cmake.sh googletest > /dev/null
-
 printf "Build Celero\n"
-./build_using_cmake.sh Celero "-DCELERO_COMPILE_DYNAMIC_LIBRARIES:BOOL=OFF" > /dev/null
+./build_using_cmake.sh Celero "-DCELERO_COMPILE_DYNAMIC_LIBRARIES:BOOL=OFF -DCELERO_ENABLE_EXPERIMENTS:BOOL=OFF" > /dev/null
 
 printf "Build cereal\n"
 ./build_using_cmake.sh cereal "-DJUST_INSTALL_CEREAL=TRUE" > /dev/null 
@@ -31,14 +26,17 @@ printf "Build zlib\n"
 printf "build rocksdb"
 ./build_rocksdb.sh > /dev/null 
 
-printf "Build lz4"
+printf "Build lz4\n"
 ./build_using_make.sh lz4 > /dev/null 
 
-printf "Build bzip2"
-./build_using_make.sh bzip2 > /dev/null 
-
-printf "Build jemalloc"
+printf "Build jemalloc\n"
 ./build_using_autogen.sh jemalloc > /dev/null
 
-printf "Build SQLite"
-./build_using_configure.sh sqlite "CFLAGS=-O3"
+# Download required packages
+./download_source_code.sh http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz bzip2
+# ./download_source_code.sh http://sqlite.org/2017/sqlite-autoconf-3180000.tar.gzprintf "Build SQLite"
+
+# ./build_using_configure.sh sqlite "CFLAGS=-O3"
+
+# printf "Build bzip2\n"
+# ./build_using_make.sh bzip2 > /dev/null 
