c18d082ec61b59e3ca486fa51a7dcdf864b231d0
Hung Dang [hdang] <hdang@athenahealth.com>
2018-07-09 22:45:55 -0400

    Update rocksdb build script.

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-source ./get_build_options.sh
-ROCKSDB_DIR="$SRC_DIR/rocksdb"
 
-# Build rocksdb
-pushd $ROCKSDB_DIR
-git clean -df
-make clean
-make DEBUG_LEVEL=0 $BUILD_OPTS static_lib EXTRA_CFLAGS="$EXTRA_CFLAGS" EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS}"
+./build_using_cmake.sh rocksdb "-DWITH_TESTS=OFF -DWITH_LZ4=ON -DWITH_BZ2=OFF -DWITH_ZLIB=OFF -DWITH_ZSTD=OFF -DWITH_SNAPPY=OFF"
 
-# Remove debuging symbols 
-osType=$(uname)
-case "$osType" in
-    "Darwin")
-        {
-			echo "Do not strip debug symbols in MacOS";
-        } ;;
-    "Linux")
-        {
-			strip -g librocksdb.a
-        } ;;
-    *)
-        {
-            echo "Unsupported OS, exiting"
-            exit
-        } ;;
-esac
-popd;
+# source ./get_build_options.sh
+# ROCKSDB_DIR="$SRC_DIR/rocksdb"
 
-# Install libraries
-cp $ROCKSDB_DIR/librocksdb.a lib/
-cp $ROCKSDB_DIR/include/rocksdb include/ -rv 
+# # Build rocksdb
+# pushd $ROCKSDB_DIR
+# git clean -df
+# make clean
+# make DEBUG_LEVEL=0 $BUILD_OPTS static_lib EXTRA_CFLAGS="$EXTRA_CFLAGS" EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS}"
+
+# # Remove debuging symbols 
+# osType=$(uname)
+# case "$osType" in
+#     "Darwin")
+#         {
+# 			echo "Do not strip debug symbols in MacOS";
+#         } ;;
+#     "Linux")
+#         {
+# 			strip -g librocksdb.a
+#         } ;;
+#     *)
+#         {
+#             echo "Unsupported OS, exiting"
+#             exit
+#         } ;;
+# esac
+# popd;
+
+# # Install libraries
+# cp $ROCKSDB_DIR/librocksdb.a lib/
+# cp $ROCKSDB_DIR/include/rocksdb include/ -rv 
