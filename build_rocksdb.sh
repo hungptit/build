d76a52f33353b7fcae7032b0416bf9264270a29e
Hung Dang <hungptit@gmail.com>
2018-07-11 16:02:46 -0400

    Update RocksDB build script

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -8,25 +8,6 @@ git clean -df
 make clean
 make DEBUG_LEVEL=0 $BUILD_OPTS static_lib EXTRA_CFLAGS="$EXTRA_CFLAGS" EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS}"
 
-# Remove debuging symbols 
-osType=$(uname)
-case "$osType" in
-    "Darwin")
-        {
-			echo "Do not strip debug symbols in MacOS";
-        } ;;
-    "Linux")
-        {
-			strip -g librocksdb.a
-        } ;;
-    *)
-        {
-            echo "Unsupported OS, exiting"
-            exit
-        } ;;
-esac
-popd;
-
 # Install libraries
 cp $ROCKSDB_DIR/librocksdb.a lib/
-cp $ROCKSDB_DIR/include/rocksdb include/ -rv 
+cp -rv $ROCKSDB_DIR/include/rocksdb include/
