36194fe501eaf15fb4ea2a89906a5ca9e28b07d5
Hung Dang [hdang] <hdang@athenahealth.com>
2018-07-11 01:00:21 -0400

    Update RocksDB build script.

--- build_rocksdb.sh
+++ build_rocksdb.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
-
-./build_using_cmake.sh rocksdb "-DWITH_TESTS=OFF -DWITH_LZ4=ON -DWITH_BZ2=OFF -DWITH_ZLIB=OFF -DWITH_ZSTD=OFF -DWITH_SNAPPY=OFF"
-
-# source ./get_build_options.sh
-# ROCKSDB_DIR="$SRC_DIR/rocksdb"
-
-# # Build rocksdb
-# pushd $ROCKSDB_DIR
-# git clean -df
-# make clean
-# make DEBUG_LEVEL=0 $BUILD_OPTS static_lib EXTRA_CFLAGS="$EXTRA_CFLAGS" EXTRA_CXXFLAGS="${EXTRA_CXXFLAGS}"
-
-# # Remove debuging symbols 
-# osType=$(uname)
-# case "$osType" in
-#     "Darwin")
-#         {
-# 			echo "Do not strip debug symbols in MacOS";
-#         } ;;
-#     "Linux")
-#         {
-# 			strip -g librocksdb.a
-#         } ;;
-#     *)
-#         {
-#             echo "Unsupported OS, exiting"
-#             exit
-#         } ;;
-# esac
-# popd;
-
-# # Install libraries
-# cp $ROCKSDB_DIR/librocksdb.a lib/
-# cp $ROCKSDB_DIR/include/rocksdb include/ -rv 
+# TODO: Enable compression
+./build_using_cmake.sh rocksdb "-DWITH_TESTS=OFF -DWITH_LZ4=OFF -DWITH_BZ2=OFF -DWITH_ZLIB=OFF -DWITH_ZSTD=OFF -DWITH_SNAPPY=OFF -DWITH_JEMALLOC=OFF -DCMAKE_INSTALL_LIBDIR=lib"
